4af5b0da00df2cfeb1db5a8eb750e890272d0b69
baniyapratik <baniyapratik@gmail.com>
2023-02-28 21:53:27 -0800

    add auth middleware and router for it

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -22,7 +22,6 @@ func (app *Config) routes() http.Handler {
 	mux.Get("/register", app.RegisterPage)
 	mux.Post("/register", app.PostRegisterPage)
 	mux.Get("/activate", app.ActivateAccount)
-	mux.Get("/plans", app.ChooseSubscription)
 
 	mux.Get("/test-email", func(w http.ResponseWriter, r *http.Request) {
 		m := Mail{
@@ -41,6 +40,16 @@ func (app *Config) routes() http.Handler {
 		}
 		m.sendMail(msg, make(chan error))
 	})
+
+	mux.Mount("/members", app.authRouter())
+	return mux
+}
+
+func (app *Config) authRouter() http.Handler {
+	mux := chi.NewRouter()
+	mux.Use(app.Auth)
+	mux.Get("/plans", app.ChooseSubscription)
+	mux.Get("/subscribe", app.SubscribeToPlan)
 
 	return mux
 }
