1444f6604b6584afae903394e306a87b67f2dc56
baniyapratik <baniyapratik@gmail.com>
2023-02-28 21:07:30 -0800

    activating user

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -111,7 +111,30 @@ func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {
 
 func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
 	// validate url
-
+	url := r.RequestURI
+	testURL := fmt.Sprintf("http://localhost%s", url)
+	okay := VerifyToken(testURL)
+	if !okay {
+		app.Session.Put(r.Context(), "error", "invalid token")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	// activate account
+	u, err := app.Models.User.GetByEmail(r.URL.Query().Get("email"))
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "no user found")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	u.Active = 1
+	err = u.Update()
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "unable to update the user")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	app.Session.Put(r.Context(), "flash", "account activated. you can now login")
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
 	// generate an invoice
 
 	// send an email with attachments
