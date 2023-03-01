c406245973a1b2c8e7aeccb903083de81a6ae4e3
baniyapratik <baniyapratik@gmail.com>
2023-02-28 22:04:42 -0800

    generating an invoice

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -162,6 +162,25 @@ func (app *Config) SubscribeToPlan(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// generate an invoice
+	app.Wait.Add(1)
+
+	go func() {
+		defer app.Wait.Done()
+
+		invoice, err := app.getInvoice(user, plan)
+		if err != nil {
+			// send this to a chanel
+			app.ErrorChan <- err
+		}
+
+		msg := Message{
+			To:       user.Email,
+			Subject:  "your invoice",
+			Data:     invoice,
+			Template: "invoice",
+		}
+		app.sendEmail(msg)
+	}()
 
 	// send an email with attachments
 
@@ -174,6 +193,9 @@ func (app *Config) SubscribeToPlan(w http.ResponseWriter, r *http.Request) {
 	// redirect
 }
 
+func (app *Config) getInvoice(u data.User, plan *data.Plan) (string, error) {
+	return plan.PlanAmountFormatted, nil
+}
 func (app *Config) ChooseSubscription(w http.ResponseWriter, r *http.Request) {
 	plans, err := app.Models.Plan.GetAll()
 	if err != nil {
@@ -187,4 +209,3 @@ func (app *Config) ChooseSubscription(w http.ResponseWriter, r *http.Request) {
 		Data: dataMap,
 	})
 }
-
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -39,12 +39,14 @@ func main() {
 	wg := sync.WaitGroup{}
 	// set up the application config
 	app := Config{
-		Session:  session,
-		DB:       db,
-		Wait:     &wg,
-		InfoLog:  infoLog,
-		ErrorLog: errorLog,
-		Models:   data.New(db),
+		Session:       session,
+		DB:            db,
+		Wait:          &wg,
+		InfoLog:       infoLog,
+		ErrorLog:      errorLog,
+		Models:        data.New(db),
+		ErrorChan:     make(chan error),
+		ErrorChanDone: make(chan bool),
 	}
 	// set up mail
 	app.Mailer = app.createMail()
@@ -53,10 +55,21 @@ func main() {
 	// listen for signals
 	go app.listenForShutdown()
 
+	// listen for errors
+	go app.listenForErrors()
 	// listen for web connections
 	app.serve()
 }
-
+func (app *Config) listenForErrors() {
+	for {
+		select {
+		case err := <-app.ErrorChan:
+			app.ErrorLog.Println(err)
+		case <-app.ErrorChanDone:
+			return
+		}
+	}
+}
 func (app *Config) serve() {
 	// start http server
 	srv := &http.Server{
@@ -159,11 +172,14 @@ func (app *Config) shutdown() {
 	app.Wait.Wait()
 
 	app.Mailer.DoneChan <- true
+	app.ErrorChanDone <- true
 
 	app.InfoLog.Println("closing channels and shutting down application...")
 	close(app.Mailer.MailerChan)
 	close(app.Mailer.ErrorChan)
 	close(app.Mailer.DoneChan)
+	close(app.ErrorChanDone)
+	close(app.ErrorChan)
 }
 
 func (app *Config) createMail() Mail {
